b81f3309b84a9bb6c044e693133062b66a90130e
kamalesh-pathy-va <asokankamalesh@gmail.com>
2023-07-03 23:25:33 +0530

    feature container remaining for desktop

--- index.js
+++ index.js
@@ -29,35 +29,83 @@ tracks.forEach(track => {
     
     const slideWidth = slides[0].getBoundingClientRect().width;
     slides.forEach((slide, index) => {
-        console.log(slide);
         slide.style.left = slideWidth * index + 'px';
     });
     
     nextBtn.addEventListener("click", e => {
         const activeSlide = track.querySelector('.active-slide');
         const nextSlide = activeSlide.nextElementSibling;
+
         const activeContent = activeSlide.querySelector('.active-content');
         const nextContent = nextSlide.querySelector('.main-image-content');
+
         const amountToMove = nextSlide.style.left;
         track.style.transform = 'translateX(-' + amountToMove + ')';
         activeSlide.classList.remove('active-slide');
         nextSlide.classList.add('active-slide');
         activeContent.classList.remove('active-content');
         nextContent.classList.add('active-content');
-        nextContent.style.setProperty('--animation-name', 'right-left');
+        nextContent.style.setProperty('--_animation-name', 'right-left');
     });
     
     prevBtn.addEventListener("click", e => {
         const activeSlide = track.querySelector('.active-slide');
         const prevSlide = activeSlide.previousElementSibling;
+
         const activeContent = activeSlide.querySelector('.active-content');
         const prevContent = prevSlide.querySelector('.main-image-content');
+
         const amountToMove = prevSlide.style.left;
         track.style.transform = 'translateX(-' + amountToMove + ')';
         activeSlide.classList.remove('active-slide');
         prevSlide.classList.add('active-slide');
         activeContent.classList.remove('active-content');
         prevContent.classList.add('active-content');
-        prevContent.style.setProperty('--animation-name', 'left-right');
+        prevContent.style.setProperty('--_animation-name', 'left-right');
     });
+});
+
+nextBtn.addEventListener("click", () => {
+    const activeText = document.querySelector('.active-text');
+    const nextText = activeText.nextElementSibling;
+
+    const activeFeature = document.querySelector('.active-feature');
+    const nextFeature = activeFeature.nextElementSibling;
+
+    activeText.classList.remove('active-text');
+    nextText.classList.add('active-text');
+
+    activeFeature.classList.remove('active-feature');
+    nextFeature.classList.add('active-feature');
+
+    nextText.style.setProperty('--_animation-name', 'right-left');
+    nextFeature.style.setProperty('--_animation-name', 'right-left');
+
+    prevBtn.disabled = false;
+    if (nextText.nextElementSibling === null) {
+        nextBtn.disabled = true;
+        prevBtn.disabled = false;
+    }
+});
+
+prevBtn.addEventListener("click", () => {
+    const activeText = document.querySelector('.active-text');
+    const prevText = activeText.previousElementSibling;
+    
+    const activeFeature = document.querySelector('.active-feature');
+    const prevFeature = activeFeature.previousElementSibling;
+
+    activeText.classList.remove('active-text');
+    prevText.classList.add('active-text');
+    
+    activeFeature.classList.remove('active-feature');
+    prevFeature.classList.add('active-feature');
+
+    prevText.style.setProperty('--_animation-name', 'left-right');
+    prevFeature.style.setProperty('--_animation-name', 'left-right');
+    nextBtn.disabled = false;
+    if (prevText.previousElementSibling === null) {
+        nextBtn.disabled = false;
+        prevBtn.disabled = true;
+    }
 });
